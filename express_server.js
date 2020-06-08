34cd0f7da50f50055f45930e8de3b48ad5adc183
Hannah Bregman <bregmanhannah@gmail.com>
2020-06-08 21:14:55 +0000

    strted up urls_index by including html skeleton

--- express_server.js
+++ express_server.js
@@ -12,9 +12,15 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
